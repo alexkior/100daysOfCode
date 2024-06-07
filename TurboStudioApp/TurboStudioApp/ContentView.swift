093011020ec96fe883f38ab031313704a1aaf6ce
Alex Kior <alexkior@MBP-Kior.home>
2024-06-07 18:26:51 +0200

    02.

--- TurboStudioApp/TurboStudioApp/ContentView.swift
+++ TurboStudioApp/TurboStudioApp/ContentView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                Text("Turbo Tandoori")
-                    .padding(.top, 30)
-                    .frame(maxWidth: .infinity)
-                    .multilineTextAlignment(.center)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .center) {
+                    Text("Turbo Tandoori")
+                        .padding(.top, 30)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Gruvilicious", size: 54))
+
+                    ScrollView(.vertical, showsIndicators: false) {
+                        if showDetails {
+                            VStack {
+                                Text("Calendar here")
+                                    .font(.custom("Gruvilicious", size: 32))
+                                    .padding()
+                            }
+                            .frame(width: 320, height: 550)
+                            .background(Color.white)
+                            .cornerRadius(20)
+
+                        }
+                    }
+                    .frame(maxHeight: .infinity)
+                    
+                    Spacer()
+                    
+                    if !showDetails {
+                        Button("Book now") {
+                            showDetails.toggle()
+                        }
+                        .frame(width: 320, height: 50)
+                        .foregroundColor(.black)
+                        .font(.custom("Gruvilicious", size: 32))
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    } else {
+                        Button("Back") {
+                            showDetails.toggle()
+                        }
+                        .frame(width: 320, height: 50)
+                        .foregroundColor(.black)
+                        .font(.custom("Gruvilicious", size: 32))
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .padding(.bottom, 20)
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: geometry.size.height)
             }
         }
         .background(
